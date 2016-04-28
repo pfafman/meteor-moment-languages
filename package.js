74c8baeb66cdf2032ef413688d818a5ef44cd023
Timothy Pfafman <tim@pfafman.com>
2016-04-28 10:09:37 -0700

    capitalize CZ and add HU

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "pfafman:moment-languages",
   summary: "Some Languages files for Moment.js. See http://momentjs.com.",
-  version: "2.11.1",
+  version: "2.11.1_1",
   git: "https://github.com/pfafman/meteor-moment-languages.git",
 });
 
