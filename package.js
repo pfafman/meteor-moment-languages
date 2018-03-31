2acf2739a160e86c067ab600320ff7ef6250b680
Timothy Pfafman <tim@pfafman.com>
2018-03-31 07:13:45 -0600

    constraints

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: "pfafman:moment-languages",
   summary: "Some Languages files for Moment.js. See http://momentjs.com.",
-  version: "2.11.1_4",
+  version: "2.11.1_5",
   git: "https://github.com/pfafman/meteor-moment-languages.git",
 });
 
 Package.on_use(function (api, where) {
-  api.versionsFrom("1.4.1");
+  //api.versionsFrom("1.4.1");
   //api.use('momentjs:moment');
   
   api.use(['tmeasday:check-npm-versions@0.3.1', 'ecmascript'],['client', 'server']);
